377b43c6a928e445278e7054c0764a7a44233f46
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-11 16:34:32 +0530

    refactored: Maximum of three comparable objects

--- MaximumOfThreeNumbers.java
+++ MaximumOfThreeNumbers.java
@@ -2,7 +2,7 @@ package com.bridgelabz.generics;
 
 public class MaximumOfThreeNumbers {
 
-	// Determines the largest of three comparable String values
+	// Determines the largest of three comparable objects
 	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
 		T max = x; // assume x is initially the largest
 
@@ -17,6 +17,9 @@ public class MaximumOfThreeNumbers {
 	}
 
 	public static void main(String[] args) {
+		System.out.printf("Maximum of %d, %d and %d is %d\n\n", 10, 3, 5, maximum(3, 5, 10));
+
+		System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.5, 2.3, 7.8, maximum(6.5, 2.3, 7.8));
 
 		System.out.printf("Maximum of %s, %s and %s is %s\n\n", "Apple", "Peach", "Banana", maximum("Apple", "Peach", "Banana"));
 	}
