3921216ca410c9dbfc7f0086207b35fa779835e8
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-11 15:08:51 +0530

    added: Displaying start message

---
+++ MaximumOfThreeNumbers.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.generics;
+
+public class MaximumOfThreeNumbers {
+	
+	public static void main(String[] args) {
+		System.out.println("Maximum of three Numbers using Generics concept");
+	}
+
+}
