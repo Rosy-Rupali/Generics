21b97dae5818da00396ba42e52b4e2e72fa89ccb
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-11 16:57:27 +0530

    refactored: Maximum of three comparable objects in Generic class

--- MaximumOfThreeNumbers.java
+++ MaximumOfThreeNumbers.java
@@ -1,9 +1,22 @@
 package com.bridgelabz.generics;
 
-public class MaximumOfThreeNumbers {
+public class MaximumOfThreeNumbers <T extends Comparable<T>> {
+
+	T x, y, z;
+
+	// Parameterised constructor
+	public MaximumOfThreeNumbers(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public T getMaximum() {
+		return MaximumOfThreeNumbers.getMaximum(x, y, z);
+	}
 
 	// Determines the largest of three comparable objects
-	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+	public static <T extends Comparable<T>> T getMaximum(T x, T y, T z) {
 		T max = x; // assume x is initially the largest
 
 		if (y.compareTo(max) > 0) {
@@ -14,14 +27,6 @@ public class MaximumOfThreeNumbers {
 			max = z; // z is the largest
 		}
 		return max; // return the largest integer
-	}
-
-	public static void main(String[] args) {
-		System.out.printf("Maximum of %d, %d and %d is %d\n\n", 10, 3, 5, maximum(3, 5, 10));
-
-		System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.5, 2.3, 7.8, maximum(6.5, 2.3, 7.8));
-
-		System.out.printf("Maximum of %s, %s and %s is %s\n\n", "Apple", "Peach", "Banana", maximum("Apple", "Peach", "Banana"));
 	}
 
 }
