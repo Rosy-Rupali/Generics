bf945f279e3750ab12eee7affe882fa58baee44b
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-11 17:52:02 +0530

    refactored: added printf statements

--- MaximumOfThreeNumbers.java
+++ MaximumOfThreeNumbers.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.generics;
 
-public class MaximumOfThreeNumbers <T extends Comparable<T>> {
+public class MaximumOfThreeNumbers<T extends Comparable<T>> {
 
 	T x, y, z;
 
@@ -15,7 +15,6 @@ public class MaximumOfThreeNumbers <T extends Comparable<T>> {
 		return MaximumOfThreeNumbers.getMaximum(x, y, z);
 	}
 
-	// Determines the largest of three comparable objects
 	public static <T extends Comparable<T>> T getMaximum(T x, T y, T z) {
 		T max = x; // assume x is initially the largest
 
@@ -29,4 +28,12 @@ public class MaximumOfThreeNumbers <T extends Comparable<T>> {
 		return max; // return the largest integer
 	}
 
+	public static void main(String[] args) {
+		System.out.printf("Maximum of %d, %d and %d is %d\n\n", 10, 3, 5, getMaximum(3, 5, 10));
+
+		System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.5, 2.3, 7.8, getMaximum(6.5, 2.3, 7.8));
+
+		System.out.printf("Maximum of %s, %s and %s is %s\n\n", "Apple", "Peach", "Banana",
+				getMaximum("Apple", "Peach", "Banana"));
+	}
 }
