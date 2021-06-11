724a243dd889c7407df8ce0df05e9a201aed6dcb
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-11 15:14:16 +0530

    added: Maximum of three integer numbers

--- MaximumOfThreeNumbers.java
+++ MaximumOfThreeNumbers.java
@@ -1,9 +1,24 @@
 package com.bridgelabz.generics;
 
 public class MaximumOfThreeNumbers {
-	
+
+	// Determines the largest of three comparable integers
+	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+		T max = x; // assume x is initially the largest
+
+		if (y.compareTo(max) > 0) {
+			max = y; // y is the largest so far
+		}
+
+		if (z.compareTo(max) > 0) {
+			max = z; // z is the largest
+		}
+		return max; // return the largest integer
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Maximum of three Numbers using Generics concept");
+
+		System.out.printf("Maximum of %d, %d and %d is %d\n\n", 10, 3, 5, maximum(3, 5, 10));
 	}
 
 }
