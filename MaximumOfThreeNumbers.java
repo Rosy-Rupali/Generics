c28754234ec574140f978a6ff80917978acf213d
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-12 02:57:10 +0530

    refactored: Added generics

--- MaximumOfThreeNumbers.java
+++ MaximumOfThreeNumbers.java
@@ -1,5 +1,5 @@
 /**************************************************
- * Purpose :  It is a program to find maximum number using Generics
+ * Purpose :  It is a program to print the JSON input file to Java String type
  * @author Rosy Rupali
  * @since 11-06-2021
  * @version 1.0
@@ -8,11 +8,11 @@
 package com.bridgelabz.generics;
 
 public class MaximumOfThreeNumbers<T extends Comparable<T>> {
-	// Taking more than three values in the form of an array
+	// Defines array of generic elements
 	T[] inputArray;
 
 	public MaximumOfThreeNumbers(T[] inputArray) {
-		this.inputArray = inputArray;
+		this.inputArray = inputArray; // takes the array of elements
 	}
 
 	public T getMaximum() {
@@ -35,18 +35,20 @@ public class MaximumOfThreeNumbers<T extends Comparable<T>> {
 	}
 
 	public static <T> void printMaximum(T[] inputArray, T max) {
+
 		for (T element : inputArray)
 			System.out.print("  " + element);
 		System.out.println("\nMaximum of the present element is: " + max);
 	}
 
 	public static void main(String args[]) {
+
 		Integer[] intArray = { 4, 2, 1, 18, 34, 7, 9 };
 		Float[] floatArray = { 6.5f, 2.9f, 2.6f, 4.6f };
 		String[] stringArray = { "Apple", "Grapes", "Pineapple", "Orange", "Banana", "Lemon" };
-		new MaximumOfThreeNumbers(intArray).getMaximum();
-		new MaximumOfThreeNumbers(floatArray).getMaximum();
-		new MaximumOfThreeNumbers(stringArray).getMaximum();
+		new MaximumOfThreeNumbers<Integer>(intArray).getMaximum();
+		new MaximumOfThreeNumbers<Float>(floatArray).getMaximum();
+		new MaximumOfThreeNumbers<String>(stringArray).getMaximum();
 	}
 
 }
