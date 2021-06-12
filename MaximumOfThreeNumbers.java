15c90afe7d8138254b4154531d8cfd0a43708a4e
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-12 10:13:07 +0530

    modified: added documentation

--- MaximumOfThreeNumbers.java
+++ MaximumOfThreeNumbers.java
@@ -1,8 +1,21 @@
+/**************************************************
+ * Purpose :  It is a program to print the maximum using Generics
+ * @author Rosy Rupali
+ * @since 11-06-2021
+ * @version 1.0
+ *************************************************/
 package com.bridgelabz.generics;
 
 public class MaximumOfThreeNumbers {
 
 	// Determines the largest of three comparable Float values
+	/**
+	 * @param <T>
+	 * @param x first argument
+	 * @param y second argument 
+	 * @param z third argument
+	 * @return maximum value in float
+	 */
 	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
 		T max = x; // assume x is initially the largest
 
@@ -18,7 +31,7 @@ public class MaximumOfThreeNumbers {
 
 	public static void main(String[] args) {
 
-		System.out.printf("Maximum of %.1f, %.1f and %.1f is %.2f\n\n", 6.5, 2.3, 7.8, maximum(6.5, 2.3, 7.8));
+		System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.5, 2.3, 7.8, maximum(6.5, 2.3, 7.8));
 	}
 
 }
