cd4d1ac98a9461d1e389fe2647b53f9ce8373f50
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-12 10:09:15 +0530

    modified: added documentation

--- MaximumOfThreeNumbers.java
+++ MaximumOfThreeNumbers.java
@@ -1,8 +1,21 @@
+/**************************************************
+ * Purpose :  It is a program to print the maximum using Generics
+ * @author Rosy Rupali
+ * @since 11-06-2021
+ * @version 1.0
+ *************************************************/
 package com.bridgelabz.generics;
 
 public class MaximumOfThreeNumbers {
 
 	// Determines the largest of three comparable integers
+	/**
+	 * @param <T>
+	 * @param x first integer
+	 * @param y second integer
+	 * @param z third integer
+	 * @return maximum value
+	 */
 	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
 		T max = x; // assume x is initially the largest
 
